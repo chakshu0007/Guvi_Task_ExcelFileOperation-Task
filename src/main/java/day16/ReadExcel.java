69b7997daed7553a3a8bbebdfc49ef6888a14460
chakshu0007 <chakshu0007@gmail.com>
2024-10-16 19:55:32 +0530

    task8

---
+++ src/main/java/day16/ReadExcel.java
@@ -0,0 +1,55 @@
+package day16;
+
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcel {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		// Open the work book
+		XSSFWorkbook book = new XSSFWorkbook("C:\\Users\\chakshug\\eclipse-workspace\\ExcelFileOperation\\src\\main\\java\\day16\\Studentdetails.xlsx");
+		
+		//GEt to the sheet
+		XSSFSheet sheet = book.getSheet("Details");
+		
+		//get the no. of rows
+		int rowCount = sheet.getLastRowNum();
+		
+		// get the no. of columns - for that cursor will go to first row and then to last col to count
+		int columnCount = sheet.getRow(0).getLastCellNum();
+		
+		// create 2D array
+		String[][] data = new String[rowCount][columnCount];
+		
+		// get into row
+		for (int i=1; i<=rowCount; i++) {
+			XSSFRow row = sheet.getRow(i);
+			
+			// get into columns
+			for(int j =0; j<columnCount; j++) {
+				XSSFCell cell = row.getCell(j);
+				
+				// get the value - read the value
+			// System.out.println(cell.getStringCellValue()); // only method to read the values - so in Excel do Fixed Width from Data > Text to Columns menu
+				
+				//to store in a array
+				data[i-1][j] =cell.getStringCellValue();
+				}
+		// System.out.println();
+		}
+		for (String[] row : data) {
+			for (String x : row){
+				System.out.println(x +" ");
+			}
+		}
+		book.close();
+		
+	}
+
+}
---
+++ src/main/java/task8/WriteExcel.java
@@ -0,0 +1,71 @@
+package task8;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class WriteExcel {
+	public static void main(String[] args) throws IOException {
+		
+		// First create a work book
+		XSSFWorkbook book = new XSSFWorkbook();
+		
+		// Create the sheet inside book
+		XSSFSheet sheet = book.createSheet("Sheet1");
+		
+		// Store the details -> Name , Age , City 
+		// using Object because in ArrayList this Object store all tyes of datatype
+		// [][] rows and columns
+		Object[][] data = {
+				{"Name","Age","Email"},
+				{"John Doe",30,"john@test.com"},
+				{"Bane Doe",28,"john@test.com"},
+				{"Bob Smith",35,"jacky@example.com"},
+				{"Swapnil",37,"swapnil@example.com"}
+		};
+		
+		// initially set row count to 0, everytime we enter data in 1 row rowCount is going to increase
+		int rowCount = 0;
+		
+		// using for each loop to get into each row
+		// focusing on 1 row will give 1-d array 
+		for(Object[] row1 : data) {  		 // this means for each row in data
+			// now get into each cell
+			XSSFRow row = sheet.createRow(rowCount++);
+			
+			int columnCount = 0;
+			
+			for(Object col : row1) {		// this means for each column in a row
+			
+			XSSFCell cell =	row.createCell(columnCount++);
+			
+			// checking the type of data and making the entry
+			if(col instanceof String) {
+				cell.setCellValue((String)col); // here converting the col from Object to String
+				
+			}else if(col instanceof Integer) {
+				cell.setCellValue((Integer)col); // because only 2 dataypes are present,else need to convert all
+			}
+			}
+		}
+		// fileoutputstream to write in xlsx with path given '\\'
+	try {
+		FileOutputStream output = new FileOutputStream("C:\\Users\\chakshug\\eclipse-workspace\\ExcelFileOperation\\src\\main\\java\\task8\\Employeedetails.xlsx");
+			// If file found then write in the book
+		book.write(output);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	
+	}
+	// IMP to CLOSE THE OPENED BOOK
+	book.close();
+	
+	}
+
+}
+
